955318eb29fa8ca947ff25db35e8a07be76360c0
Maxime Dréau <maxime@dreau.fr>
2017-01-15 16:24:55 +0100

    Déplacement aléatoire des bots

--- src/scripts/components/PlayerBot.js
+++ src/scripts/components/PlayerBot.js
@@ -1,6 +1,13 @@
 const PlayerBot = WS.Components.PlayerBot = class Player extends WS.Components.Player {
     constructor(options) {
         super(options);
+
+        this.movement = {
+          x: 0,
+          y: 0,
+        };
+
+        this.updateBotDirection();
     }
     registerGamepadButtons() {
         console.log('registerGamepadButtons bot player%s', this.playerNumber);
@@ -9,15 +16,18 @@ const PlayerBot = WS.Components.PlayerBot = class Player extends WS.Components.P
         }, 1000);
     }
     update() {
-      if (this.pad.connected && this.sprite.alive) {
-          let moveX = _.random(-1, 1, true);
-          let moveY = _.random(-1, 1, true);
-          if (moveX || moveY) {
-              this.sprite.rotation = Math.atan2(moveY, moveX);
-              this.sprite.body.x += moveX * WS.Config.PlayerSpeed;
-              this.sprite.body.y += moveY * WS.Config.PlayerSpeed;
-          }
+      if (this.movement.x || this.movement.y) {
+          this.sprite.rotation = Math.atan2(this.movement.y, this.movement.x);
+          this.sprite.body.x += this.movement.x * WS.Config.PlayerSpeed;
+          this.sprite.body.y += this.movement.y * WS.Config.PlayerSpeed;
       }
       this.indicator.animations.frame = this.pad.connected ? 0 : 1;
+    }
+
+    updateBotDirection() {
+      this.movement.x = _.random(-1, 1, true);
+      this.movement.y = _.random(-1, 1, true);
+      const nextMovementDelay = _.random(500, 1500, true);
+      setTimeout(this.updateBotDirection.bind(this), nextMovementDelay);
     }
 }
