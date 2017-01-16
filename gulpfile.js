df246f58c8754a71ebd9dc49b224923d0bd453d8
Maxime Dréau <maxime@dreau.fr>
2017-01-16 21:06:00 +0100

    Migration vers le générateur fountain-webapp avec gulp et webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,115 +1,29 @@
-var gulp = require('gulp'),
-  $ = require('gulp-load-plugins')(),
-  bowerFiles = require('main-bower-files'),
-  concat = require('gulp-concat-sourcemap'),
-  deploy = require('gulp-gh-pages'),
-  del = require('del'),
-  runSequence = require('run-sequence');
+const gulp = require('gulp');
+const HubRegistry = require('gulp-hub');
+const browserSync = require('browser-sync');
 
-var paths = {
-  assetsSvg: 'assets/**/*.svg',
-  assets: 'src/assets/**/*',
-  less: 'src/css/main.less',
-  index: 'src/index.html',
-  js: ['src/scripts/_init.js', 'src/scripts/lib/*.js', 'src/scripts/**/*.js'],
-  build: 'build',
-  dist: 'dist'
-};
+const conf = require('./conf/gulp.conf');
 
-gulp.task('clean', function (cb) {
-  return del([paths.build, paths.dist], cb);
-});
+// Load some files into the registry
+const hub = new HubRegistry([conf.path.tasks('*.js')]);
 
-gulp.task('copy', function () {
-  return gulp.src(paths.assets)
-    .pipe(gulp.dest(paths.dist + '/assets'));
-});
+// Tell gulp to use the tasks just loaded
+gulp.registry(hub);
 
-gulp.task('svg2png', function () {
-  gulp.src(paths.assetsSvg)
-    .pipe($.debug({title: 'svg:'}))
-    .pipe($.svg2png())
-    .pipe(gulp.dest(paths.build + '/assets'));
-});
+gulp.task('build', gulp.series(gulp.parallel('other', 'webpack:dist')));
+gulp.task('test', gulp.series('karma:single-run'));
+gulp.task('test:auto', gulp.series('karma:auto-run'));
+gulp.task('serve', gulp.series('webpack:watch', 'watch', 'browsersync'));
+gulp.task('serve:dist', gulp.series('default', 'browsersync:dist'));
+gulp.task('default', gulp.series('clean', 'build'));
+gulp.task('watch', watch);
 
-gulp.task('javascript', function () {
-  return gulp.src(paths.js)
-    // .pipe($.sort())
-    .pipe($.sourcemaps.init())
-    .pipe($.iife())
-    // .pipe($.babel({
-    //     presets: ['es2015']
-    // }))
-    .pipe(concat('main.js'))
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest(paths.build));
-});
+function reloadBrowserSync(cb) {
+  browserSync.reload();
+  cb();
+}
 
-gulp.task('less', function () {
-  return gulp.src(paths.less)
-    .pipe($.less())
-    .pipe(gulp.dest(paths.build));
-});
-
-gulp.task('processhtml', function () {
-  return gulp.src(paths.index)
-    .pipe($.processhtml())
-    .pipe(gulp.dest(paths.dist));
-});
-
-gulp.task('inject', ['javascript'], function () {
-  return gulp.src(paths.index)
-    .pipe($.inject(gulp.src(bowerFiles()), {name: 'bower', relative: true}))
-    .pipe(gulp.dest('src'));
-});
-
-gulp.task('reload', ['javascript'], function () {
-  gulp.src(paths.index)
-    .pipe($.connect.reload());
-});
-
-gulp.task('watch', function () {
-  gulp.watch(paths.js, ['javascript', 'reload']);
-  gulp.watch(paths.less, ['less', 'reload']);
-  gulp.watch(paths.index, ['reload']);
-  gulp.watch(paths.assetsSvg, ['svg2png', 'reload']);
-});
-
-gulp.task('connect', function () {
-  $.connect.server({
-    root: [__dirname + '/src', paths.build],
-    port: 9000,
-    livereload: false
-  });
-});
-
-gulp.task('open', function () {
-  gulp.src(paths.index)
-    .pipe($.open({uri: 'http://localhost:9000'}));
-});
-
-gulp.task('minifyJs', function () {
-  var all = bowerFiles().concat(paths.build + '/main.js');
-  return gulp.src(all)
-    .pipe($.uglifyjs('all.min.js', {outSourceMap: false}))
-    .pipe(gulp.dest(paths.dist));
-});
-
-gulp.task('minifyCss', ['less'], function () {
-  return gulp.src(paths.build + '/main.css')
-    .pipe($.minifyCss())
-    .pipe(gulp.dest(paths.dist))
-});
-
-gulp.task('deploy', function () {
-  return gulp.src('dist/**/*')
-    .pipe(deploy());
-});
-
-gulp.task('default', function () {
-  // runSequence('clean', ['inject', 'svg2png', 'less', 'connect', 'watch'], 'open');
-  runSequence('clean', ['inject', 'svg2png', 'less', 'connect', 'watch']);
-});
-gulp.task('build', function () {
-  return runSequence('clean', ['copy', 'minifyJs', 'minifyCss', 'processhtml']);
-});
+function watch(done) {
+  gulp.watch(conf.path.tmp('index.html'), reloadBrowserSync);
+  done();
+}
